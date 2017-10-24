c51b6ef4add1c28386c6a4cd5b831ad9ba442913
Cherie <c.k.fishlock@gmail.com>
2017-10-24 18:45:01 +1100

    Solved!

--- implementation/climbing-the-leaderboard.py
+++ implementation/climbing-the-leaderboard.py
@@ -1,9 +1,8 @@
 #!/bin/python3
 
 import sys
+import numpy as np
 from datetime import datetime as dt
-
-
 
 # n = int(input().strip())
 # scores = [int(scores_temp) for scores_temp in input().strip().split(' ')]
@@ -11,13 +10,13 @@ from datetime import datetime as dt
 # alice = [int(alice_temp) for alice_temp in input().strip().split(' ')]
 # your code goes here
 
+# TEST CASE 1
 n = 7
 scores = [100, 100, 50, 40, 40, 20, 10]
 m = 4
 alice = [5, 25, 50, 100]
 
-import numpy as np
-
+# TEST CASE 2
 f = open('test07.dat','r+')
 lines = f.readlines()
 n = int(lines[0])
@@ -25,15 +24,11 @@ scores = np.array(lines[1].split(' '),dtype='int')
 m = int(lines[2])
 alice = np.array(lines[3].split(' '),dtype='int')
 
-
-
 # ------------------------------
 # ------------------------------
 # ------------------------------ 
 
-
-# initialise rank list
-rank_list = [1]
+rank_list = [1]  # initialise rank list
 
 for j in range(1,n):
     if scores[j-1] == scores[j]:
@@ -41,53 +36,36 @@ for j in range(1,n):
     else: 
         rank_list.append(rank_list[j-1]+1)
 
-# min_score = min(scores)
 max_score = max(scores)
 
+scores = list(scores)
+rank_list = list(rank_list)
 
-#scores_reverse = sorted(scores,key=int,reverse=True)
-#rank_list_reverse = sorted(rank_list,key=int,reverse=True)
-#alice_reverse = sorted(alice,reverse=True)
-
-
-
-scores_reverse = list(scores)
-scores_reverse.reverse()
-
-rank_list_reverse = list(rank_list)
-rank_list_reverse.reverse()
-
-
-
-min_rank_list_reverse = rank_list_reverse[0]
 
 alice_rank = []
 
 start = dt.now()   
 
+j = len(scores)-1
+
 for i,alice_score in enumerate(alice):
 
-    if alice_score >= max_score:
-        #alice_rank.append('1')
-        pass
-        #print(1)
+    if alice_score < max_score:
+
+        
+
+        while alice_score >= scores[j]:
+
+            j = j - 1
+
+        alice_rank.append(str(rank_list[j]+1))
 
     else:
-        j = 0
-
-        while alice_score >= scores_reverse[j]:
-
-            j = j + 1
-
-        #alice_rank.append(str(rank_list_reverse[j]+1))
-        #print(rank_list_reverse[j] + 1)
+        alice_rank.append('1')
 
 
-        scores_reverse = scores_reverse[j:]
-        rank_list_reverse = rank_list_reverse[j:]
-
-#save_results = '\n'.join(alice_rank)
-#print
+save_results = '\n'.join(alice_rank)
+print(save_results)
 # ------------------------------
 # ------------------------------
 # ------------------------------
@@ -96,6 +74,6 @@ print(dt.now() - start)
 
 
 
-# f = open('results.txt','w+')
-# f.write(save_results)
-# f.close()
+f = open('results.txt','w+')
+f.write(save_results)
+f.close()
